a70f059d47d90cbb73ac9aa6ea95719c262263ee
Maxmiliano F Braga <maxmiliano@gmail.com>
2024-02-21 17:47:05 -0300

    Improve tests readability (#6)

--- metrics-backend/app/models/metric.rb
+++ metrics-backend/app/models/metric.rb
@@ -4,16 +4,16 @@
 class Metric < ApplicationRecord
   validates :timestamp, :name, :value, :metric_type, :unit, presence: true
 
-  scope :average_by_interval, ->(interval = 'minute') {
-    interval_group = sanitize_sql_for_conditions(["DATE_TRUNC(?, timestamp)", interval])
-    select("#{interval_group} as interval_group, name, AVG(value) as average_value, metric_type, unit, COUNT(*) as count")
-      .group("name, #{interval_group}, metric_type, unit")
-      .order("name ASC, interval_group desc, metric_type ASC, unit ASC")
+  scope :average_by_interval, lambda { |interval = 'minute'|
+    interval_group = sanitize_sql_for_conditions(['DATE_TRUNC(?, timestamp)', interval])
+    select(
+      "#{interval_group} as interval_group, name, AVG(value) as average_value, metric_type, unit, COUNT(*) as count"
+    )
+      .group(:name, interval_group, :metric_type, :unit)
+      .order(name: :asc, interval_group: :desc, metric_type: :asc, unit: :asc)
   }
 
-  private
-
   def self.sanitize_interval(interval)
-    %w[minute hour day].include?(interval) ? interval : 'minute' # Default to minute
+    %w[minute hour day].include?(interval) ? interval : 'minute'
   end
 end
